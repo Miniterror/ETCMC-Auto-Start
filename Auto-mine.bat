0a4270a0659f950dcad7c950fcf247c7dcbd5b15
Miniterror <101180056+Miniterror@users.noreply.github.com>
2023-12-08 16:01:12 +0100

    Update Auto-mine.bat

--- Auto-mine.bat
+++ Auto-mine.bat
@@ -2,7 +2,7 @@
 REM This script checks if there is a new version of the Auto-mine.bat file on GitHub and overwrites the local file if needed, will kill ETCMC_GETH.exe when already running and deletes the myapp.lock file if found, after these steps ti will start the ETCMC_GETH client and start mining
 
 REM Download the remote file and save it as temp.bat
-powershell -Command "Start-Process powershell -Verb runAs -ArgumentList '-Command', '\"[Net.ServicePointManager]::SecurityProtocol = \"\"tls12, tls11, tls\"\"; Invoke-WebRequest https://raw.githubusercontent.com/Miniterror/ETCMC-Auto-Start/main/Auto-mine.bat -OutFile \"\"C:\Documents and Settings\%username%\Start Menu\Programs\Startup\temp.bat\"\"\"'"
+powershell -Command "Start-Process powershell -Verb runAs -ArgumentList '-Command', '\"[Net.ServicePointManager]::SecurityProtocol = \"\"tls12, tls11, tls\"\"; Invoke-WebRequest https://raw.githubusercontent.com/Miniterror/ETCMC-Auto-Start/main/Auto-mine.bat -OutFile \"\"\"C:\Documents and Settings\%username%\Start Menu\Programs\Startup\temp.bat\"\"\"\"'"
 
 REM Compare the content of the local and remote files
 fc /b "C:\Documents and Settings\%username%\Start Menu\Programs\Startup\Auto-mine.bat" "C:\Documents and Settings\%username%\Start Menu\Programs\Startup\temp.bat" > nul
@@ -18,8 +18,8 @@ if errorlevel 1 (
   del temp.bat
 )
 
-REM Kill the ETCMC_GETH.exe process if it is running
-taskkill /f /im ETCMC_GETH.exe > nul 2>&1
+REM Kill the ETCMC_GETH.exe process and its child processes if it is running
+taskkill /f /t /im ETCMC_GETH.exe > nul 2>&1
 
 REM Change the current directory to the ETCMC_GUI\ETCMC_GETH folder
 cd /D "C:\Program Files (x86)\ETCMC ETC NODE LAUNCHER*\ETCMC_GUI\ETCMC_GETH"
